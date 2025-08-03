656e222024f16f7ccca251496917f86ade34151d
aLiZaR <azhagipparov1@gmail.com>
2025-08-04 00:58:31 +0500

    TwoMenu_and_NormolisedSpawnRate

--- Assets/Scripts/FrogPhase/CountdownBar.cs
+++ Assets/Scripts/FrogPhase/CountdownBar.cs
@@ -5,23 +5,32 @@ public class CountdownBar : MonoBehaviour
 {
     public float totalTime = 30f;
     private float timeRemaining;
-    public Image progressBar;
+    public Slider progressBar;
     public bool barIsRunning = false;
 
     void Start()
     {
         timeRemaining = totalTime;
+
+        // Убедимся, что значение слайдера от 0 до 1
+        if (progressBar != null)
+        {
+            progressBar.minValue = 0f;
+            progressBar.maxValue = 1f;
+            progressBar.value = 1f;
+        }
     }
 
     void Update()
     {
-        if (barIsRunning)
+        if (barIsRunning && timeRemaining > 0)
         {
-            if (timeRemaining > 0)
+            timeRemaining -= Time.deltaTime;
+            float progress = timeRemaining / totalTime;
+
+            if (progressBar != null)
             {
-                timeRemaining -= Time.deltaTime;
-                float progress = timeRemaining / totalTime;
-                progressBar.fillAmount = progress;
+                progressBar.value = progress;
             }
         }
     }
